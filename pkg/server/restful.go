92409d8d4194fce510b37b7c56f8b3f9e1e073da
chenxu <chenxu@infini-ai.com>
2024-08-04 01:36:36 +0800

    improve eval

--- pkg/server/restful.go
+++ pkg/server/restful.go
@@ -119,9 +119,10 @@ func (s *restfulServer) Run(ctx context.Context) error {
 	r := gin.Default()
 
 	r.GET("/api/kinds", s.getKinds)
-	r.GET("/api/eval", s.eval)
 	r.GET("/api/resource/:kind", s.getKeys)
 	r.GET("/api/resource/:kind/*key", s.getObject)
+
+	r.POST("/api/eval", s.eval)
 	r.POST("/api/resource/:kind", s.listObjects)
 
 	return r.Run(fmt.Sprintf(":%v", s.s.port))
