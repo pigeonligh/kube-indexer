1c0941a901ecbe8f79bcb1340f5035a60c47433f
chenxu <chenxu@infini-ai.com>
2024-08-05 12:18:08 +0800

    add TablePagination

--- web/src/views/list/result_table.js
+++ web/src/views/list/result_table.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,9 +7,12 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import ResultTableRow from './result_table_row';
+import { TablePagination } from '@mui/material';
 
 function ResultTable(props) {
   const headers = props.headers;
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [page, setPage] = React.useState(0);
 
   const getTableHeadRows = () => {
     const arr = [];
@@ -34,17 +38,40 @@ function ResultTable(props) {
     return <ResultTableRow headers={headers} data={row} queryFn={props.queryFn}/>
   }
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650, background: '#f7f7f7' }}>
-        <TableHead>
-          {getTableHeadRows(props)}
-        </TableHead>
-        <TableBody>
-          {props.data.map(getTableData)}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650, background: '#f7f7f7' }}>
+          <TableHead>
+            {getTableHeadRows(props)}
+          </TableHead>
+          <TableBody>
+            {props.data.slice(
+              page * rowsPerPage,
+              page * rowsPerPage + rowsPerPage,
+            ).map(getTableData)}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 20, 40]}
+        component="div"
+        count={props.data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </div>
   );
 }
 
